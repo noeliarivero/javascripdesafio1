741493ea99840839b9283e53d0452f37fad28839
Noelia Rivero <noeliariv8@gmail.com>
2021-09-15 19:17:11 -0300

    uso parseo

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
 let nombre = prompt("Decime tu nombre");
 let apellido = prompt("Ahora tu apellido");
+let edad = Number(prompt("Que edad tenes?"));
 
 alert("Hola " + nombre + " " + apellido);
+
+console.log(nombre);
+console.log(apellido);
+console.log(edad);
