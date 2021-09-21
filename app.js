54b5b5d0404b8fdf5d8e7404d31b812f75a6e13a
Noelia Rivero <noeliariv8@gmail.com>
2021-09-21 17:26:02 -0300

    desafío 2

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
-let nombre = prompt("Decime tu nombre");
-let apellido = prompt("Ahora tu apellido");
-let edad = Number(prompt("Que edad tenes?"));
+let nota = Number(prompt("Ingresá la nota que obtuviste en el examen de Ingreso"));
+const turno1 = "mañana";
+const turno2 = "tarde";
+const turno3 = "noche";    
 
-alert("Hola " + nombre + " " + apellido);
-
-console.log(nombre);
-console.log(apellido);
-console.log(edad);
+if(nota <= 5) {
+    alert("Lo sentimos no lograste aprobar el ingreso 2022 a UNL, te esperamos el próximo año!");
+} else if(nota >= 6) {
+    alert("Felicitaciones aprobaste el examen, ingresaste a la UNL.")
+    let turno = prompt("En que turno deseas cursar? Podes elegir entre los siguientes: mañana, tarde y noche");
+    turno = turno.toLowerCase()
+    if(turno === turno2 || turno === turno3) {
+        alert("Haz reservado tu turno")
+    } else if(turno = turno1) {
+        alert("Lo sentimos, no hay más cupo para el turno mañana")
+    }
+}
