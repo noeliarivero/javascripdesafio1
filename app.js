30e5c7bb0dc30796eb322728ec7471a723bcd4be
Noelia Rivero <noeliariv8@gmail.com>
2021-09-24 16:42:14 -0300

    desafío 2

--- app.js
+++ app.js
@@ -1,17 +1,12 @@
-let nota = Number(prompt("Ingresá la nota que obtuviste en el examen de Ingreso"));
-const turno1 = "mañana";
-const turno2 = "tarde";
-const turno3 = "noche";    
+let dato = Number(prompt("Ingresa el número de tu comisión"));
+let comision = 22405;
+let intentos = 3;
 
-if(nota <= 5) {
-    alert("Lo sentimos no lograste aprobar el ingreso 2022 a UNL, te esperamos el próximo año!");
-} else if(nota >= 6) {
-    alert("Felicitaciones aprobaste el examen, ingresaste a la UNL.")
-    let turno = prompt("En que turno deseas cursar? Podes elegir entre los siguientes: mañana, tarde y noche");
-    turno = turno.toLowerCase()
-    if(turno === turno2 || turno === turno3) {
-        alert("Haz reservado tu turno")
-    } else if(turno = turno1) {
-        alert("Lo sentimos, no hay más cupo para el turno mañana")
-    }
+while(dato != comision && intentos > 0){
+    alert(` El numero de comisión que ingresaste es inválido, te quedan ${intentos} intentos. `);
+    intentos--;
+    dato = Number(prompt("Ingresa el número de tu comisión"));
+} 
+if (dato === comision){
+    alert(`Ingresaste al curso Javascript.`);
 }
