b1b34be1b45c64101e65f8a5c592843af288cde9
Noelia Rivero <noeliariv8@gmail.com>
2021-09-28 15:54:00 -0300

    desafío 3

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
-let dato = Number(prompt("Ingresa el número de tu comisión"));
-let comision = 22405;
-let intentos = 3;
-
-while(dato != comision && intentos > 0){
-    alert(` El numero de comisión que ingresaste es inválido, te quedan ${intentos} intentos. `);
-    intentos--;
-    dato = Number(prompt("Ingresa el número de tu comisión"));
-} 
-if (dato === comision){
-    alert(`Ingresaste al curso Javascript.`);
+function tomarDatos(){
+    let nombre = prompt("Ingresá tu nombre");
+    let apellido = prompt("Ingresá tu apellido");
+    let lugar = prompt("En que ciudad vivís?");
+    let mostrarDatos = `Tu nombre es ${nombre} ${apellido} y tenes residís en ${lugar}`;
+    confirm(`Tu nombre es ${nombre} ${apellido} y tenes residís en ${lugar}`);
+    console.log(`Tu nombre es ${nombre} ${apellido} y tenes residís en ${lugar}`);
+    return mostrarDatos;
 }
+tomarDatos()
+
+function precio (nombre) {
+    let conjunto = nombre.toLowerCase()
+    if(conjunto == "hanna"){
+        console.log("El precio del conjunto Hanna es $2700");
+    }else if(conjunto == "frida"){
+        console.log("El precio del conjunto Frida es $2500");
+    }else if(conjunto == "greta"){
+        console.log("Lo sentimos, este producto no está disponible por el momento.");
+    }else{
+        console.log("El nombre ingresando no existe, vuelve a intentarlo.");
+    }
+}
+
+precio("nombre")
