cc93b1ffef52bcb1f758cca060a61a40fc4ed7ac
Noelia Rivero <noeliariv8@gmail.com>
2021-11-04 18:48:40 -0300

    tercera entrega proyecto

--- javascript/productos.js
+++ javascript/productos.js
@@ -1,78 +1,12 @@
 class Conjuntos{
-    constructor({nombre, precio,descripcion,img}) {
+    constructor({nombre,precio,descripcion,img,cantidad}) {
         this.nombre = nombre;
             this.precio = precio;
             this.descripcion = descripcion;
             this.img = img;
-    }
-    /*talleCorpiño(){
-            let corpiño;
-            this.corpiño = corpiño;
-            this.corpiño= Number(prompt("Ingresá tu talle del 1 al 8"));
-            if( this.corpiño >=0 && this.corpiño <=8 ){
-                console.log(`El talle del corpiño que compraste es ${this.corpiño}`);
-            }else{
-                console.log("El talle ingresado no está disponible");
-            }
-            return corpiño;
-        }
-    talleBombacha(){
-            let bombacha;
-            this.bombacha = bombacha;
-            this.bombacha = Number(prompt("Ingresá tu talle del 1 al 8"));
-            if( this.bombacha >=0 && this.bombacha <=8 ){
-                console.log(`El talle de la bombacha que compraste es ${this.bombacha}`);
-            }else{
-                console.log("El talle ingresado no está disponible");
-            }
-            return bombacha;
-        }       
-    comprar(cantidad){
-        if(this.cantidad <= 0){
-            console.log("Este producto se encuentra agotado.");
-            this.cantidad= false;
-        } else{
-            this.cantidad= this.cantidad - cantidad;
-            console.log(`Compraste ${this.nombre}, el  total es ${this.precio * cantidad}`);
-            
-        }
-        
-    }*/
-    
-}
-/*class carrito{
-    constructor({talleCorpiño,talleBombacha,cantidad}) {
-    this.talleCorpiño= talleCorpiño;
-    this.talleBombacha= talleBombacha;
-    this.cantidad= cantidad;
+            this.cantidad = cantidad;
     }
 }
-const boton     =document.getElementById("btn");
-
-const guardarTalles = () => {
-
-    const infoCarrito = new carrito({
-    talleCorpiño: document.getElementById("talleCorpiño").value,
-    talleBombacha: document.getElementById("talleBombacha").value,
-    cantidad: document.getElementById("cantidad").value,
-    })
-
-    document.getElementById("infoCarrito").innerHTML += `
-    <p>El talle de corpiño que elegiste es ${infoCarrito.talleCorpiño}</p>
-    <p>El talle de bombacha que elegiste es ${infoCarrito.talleBombacha}</p>
-    <p>La cantidad que vas a comprar es ${infoCarrito.cantidad}</p>
-    `
-    return infoCarrito;
-};
-boton.addEventListener("click", (e) => {
-    e.preventDefault()
-    guardarTalles()
-})
-boton.addEventListener("keydown", (e) => {
-    e.preventDefault()
-    guardarTalles()
-})*/
-
 const imprimirDatos =document.getElementById("printCardProducts");
 
 const conjuntoHanna = new Conjuntos("Conjunto Hanna",2370,"hola");
@@ -80,40 +14,163 @@ const conjuntoFrida = new Conjuntos("Conjunto Frida",2700,"hola");
 const conjuntoEmma = new Conjuntos("Conjunto Emma",2780,"hola");
 const conjuntoAstrid = new Conjuntos("Conjunto Astrid",2590,"hola");
 const conjuntoGreta = new Conjuntos("Conjunto Greta",2880,"hola");
-
 let listaConjuntos = [];
 
-listaConjuntos.push({ 'nombre': 'Conjunto Hanna', 'precio': 2370, 'descripcion': "hola", 'img': "../media/conjunto_hanna.jpg"}),
-listaConjuntos.push({ 'nombre': 'Conjunto Frida', 'precio': 2370, 'descripcion': "hola", 'img': "../media/frida.jpg"}),
-listaConjuntos.push({ 'nombre': 'Conjunto Emma', 'precio': 2370, 'descripcion': "hola", 'img': "../media/emma.jpg"}),
-listaConjuntos.push({ 'nombre': 'Conjunto Astrid', 'precio': 2370, 'descripcion': "hola", 'img': "../media/astrid.jpg"}),
-listaConjuntos.push({ 'nombre': 'Conjunto Greta', 'precio': 2370, 'descripcion': "hola", 'img': "../media/greta.jpg" }),
+
+listaConjuntos.push({ 'nombre': 'Conjunto Hanna', 'precio': 2370, 'descripcion': "hola", 'img': "../media/conjunto_hanna.jpg",'cantidad':10}),
+listaConjuntos.push({ 'nombre': 'Conjunto Frida', 'precio': 2370, 'descripcion': "hola", 'img': "../media/frida.jpg",'cantidad':10}),
+listaConjuntos.push({ 'nombre': 'Conjunto Emma', 'precio': 2370, 'descripcion': "hola", 'img': "../media/emma.jpg",'cantidad':10}),
+listaConjuntos.push({ 'nombre': 'Conjunto Astrid', 'precio': 2370, 'descripcion': "hola", 'img': "../media/astrid.jpg",'cantidad':10}),
+listaConjuntos.push({ 'nombre': 'Conjunto Greta', 'precio': 2370, 'descripcion': "hola", 'img': "../media/greta.jpg",'cantidad':10}),
 
 localStorage.setItem('listaConjuntos', JSON.stringify(listaConjuntos));
 let listaStorage = JSON.parse(localStorage.getItem('session'));
 
 listaConjuntos.forEach (element => {
     imprimirDatos.innerHTML += `
-        <div class="card col-3" style="width: 10rem;">
-        <img src="${element.img}" class="card-img-top" alt="...">
+    <div class="card">
+            <img src="${element.img}" class="card-img-top" alt="...">
         <div class="card-body">
-        <h5 class="card-title">${element.nombre}</h5>
-        <h6 class="card-subtitle mb-2 text-muted">${element.precio}</h6>
-        <p class="card-text">${element.descripcion}</p>
-        <button>Comprar</button>
+            <h5 class="card-title">${element.nombre}</h5>
+            <p class="card-text precio">$${element.precio}</p>
+            <button  class="btn btn-primary button">Añadir a Carrito</button>
         </div>
-        </div>
+    </div>
     `});
 
-/*function pedirConjuntos() {
-    let conjunto1 = Number(prompt("Insertá el precio del primer conjunto"));
-    let conjunto2 = Number(prompt("Insertá el precio del segundo conjunto"));
-    let sumar= (conjunto1 + conjunto2);
-    mostrarResultado(sumar);
-    return mostrarResultado;
-}
-pedirConjuntos();
+//////////////////////////////////////
+////////////CARRITO COMPRA////////////
+//////////////////////////////////////
 
- function mostrarResultado(a){
-    alert(`El total de ambos conjuntos es ${a}`),
-    console.log(`El total de ambos conjuntos es ${a}`),*/
+const clickButton = document.querySelectorAll('.button')
+const tbody = document.querySelector('.tbody')
+const clickCompra = document.querySelector('.compra')
+
+let carrito = [];
+
+clickButton.forEach(btn => {
+    btn.addEventListener('click', addToCarritoItem)
+  })
+
+function addToCarritoItem(e){
+    const button = e.target
+    const item = button.closest('.card')
+    const itemTitle = item.querySelector('.card-title').textContent;
+    const itemPrice = item.querySelector('.precio').textContent;
+    const itemImg = item.querySelector('.card-img-top').src;
+    
+    const newItem = {
+      title: itemTitle,
+      precio: itemPrice,
+      img: itemImg,
+      cantidad: 1
+    }
+  
+    addItemCarrito(newItem)
+  }
+
+function addItemCarrito(newItem){
+   carrito.push(newItem)
+   mostrarProductosAgregados()
+}
+
+function mostrarProductosAgregados(){
+    tbody.innerHTML = ''
+    carrito.map(item => {
+      const tr = document.createElement('tr')
+      tr.classList.add('ItemCarrito')
+      const Content = `
+      
+      <th scope="row">-</th>
+              <td class="table__productos">
+                <img src=${item.img}  alt="">
+                <h6 class="title">${item.title}</h6>
+              </td>
+              <td class="table__price"><p>${item.precio}</p></td>
+              <td class="table__cantidad">
+                <input type="number" min="1" value=${item.cantidad} class="input__elemento">
+                <button class="delete btn btn-danger">x</button>
+              </td>
+      
+      `
+      tr.innerHTML = Content;
+      tbody.append(tr)   
+
+    tr.querySelector(".delete").addEventListener('click', removeItemCarrito)
+    tr.querySelector(".input__elemento").addEventListener('change', sumaCantidad)
+    })
+    CarritoTotal()
+
+}
+function CarritoTotal(){
+    let Total = 0;
+    const itemCartTotal = document.querySelector('.itemCartTotal')
+    carrito.forEach((item) => {
+      const precio = Number(item.precio.replace("$", ''))
+      Total = Total + precio*item.cantidad
+    })
+
+    itemCartTotal.innerHTML = `Total $${Total}`
+    
+    addLocalStorage()
+  }
+
+  function removeItemCarrito(e){
+    const buttonDelete = e.target
+    const tr = buttonDelete.closest(".ItemCarrito")
+    const title = tr.querySelector('.title').textContent;
+    for(let i=0; i<carrito.length ; i++){
+  
+      if(carrito[i].title.trim() === title.trim()){
+        carrito.splice(i, 1)
+      }
+    }
+  
+    const alert = document.querySelector('.remove')
+  
+    setTimeout( function(){
+      alert.classList.add('remove')
+    }, 2000)
+      alert.classList.remove('remove')
+  
+    tr.remove()
+    CarritoTotal()
+  }
+  
+  function sumaCantidad(e){
+    const sumaInput  = e.target
+    const tr = sumaInput.closest(".ItemCarrito")
+    const title = tr.querySelector('.title').textContent;
+    carrito.forEach(item => {
+      if(item.title.trim() === title){
+        sumaInput.value < 1 ?  (sumaInput.value = 1) : sumaInput.value;
+        item.cantidad = sumaInput.value;
+        CarritoTotal()
+      }
+    })
+  }  
+
+  function addLocalStorage(){
+    localStorage.setItem('carrito', JSON.stringify(carrito))
+  }
+
+  window.onload = function(){
+    const storage = JSON.parse(localStorage.getItem('carrito'));
+    if(storage){
+      carrito = storage;
+      mostrarProductosAgregados()
+    }
+  }
+
+clickCompra.addEventListener('click', comprar)
+
+    function comprar (){
+        if (carrito.length > 0){
+            alert('Compra exitosa!')
+            //carrito borrar elementos del carrito
+        } else {
+            alert('No hay productos en el carrito, por favor seleccione un producto.')
+
+
+        }
+  }
